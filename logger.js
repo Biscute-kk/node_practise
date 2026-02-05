1d09b9029414d0d99b3f94572bfb813894cf62e1
Biscute-kk <arunsubedi170@gmail.com>
2026-02-05 15:03:36 +0545

    third commit

--- logger.js
+++ logger.js
@@ -1,8 +1,10 @@
+console.log(__filename);
+console.log(__dirname);
 var url='http://mylogger.io/log';
 
-function log(message){
+function logg(message){
     //send an HTTP request
     console.log(message);
 }
 
-module.exports.logger =log;
+module.exports =logg;
---
+++ test_logger.js
@@ -0,0 +1,6 @@
+console.log('test 1');
+console.log('test 2');
+function msg(){
+    console.log('wau');
+}
+module.exports.message=msg;
